a194679ad991c67f67160bf49b4c9b77d89f81a4
zhaixida <zhaixida@bytedance.com>
2020-08-28 14:56:28 +0800

    de

--- ree/ree/ViewController.m
+++ ree/ree/ViewController.m
@@ -16,7 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //
+    //--- /var/folders/m_/h2jglcmd19n5jwvg93wytl_m0000gp/T/45AAB196-2EBE-40EE-A48C-0187A42D54AA/ViewController.m/ree/ree/ViewController.m
+    +++ /var/folders/m_/h2jglcmd19n5jwvg93wytl_m0000gp/T/45AAB196-2EBE-40EE-A48C-0187A42D54AA/ViewController.m/ree/ree/ViewController.m
+    @@ -17,6 +17,9 @@
+     - (void)viewDidLoad {
+         [super viewDidLoad];
+         //
+    +    //擦擦上
+    +    //撒擦拭串
+    +   // 撒爱吃
+         // Do any additional setup after loading the view.
+     }
+     
     // Do any additional setup after loading the view.
 }
 
